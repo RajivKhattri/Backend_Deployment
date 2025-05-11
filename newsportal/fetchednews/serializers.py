654c8fa785a19dc42e2cd331806c947d6aa1e397
RajivKhattri <np03cs4a230274@heraldcollege.edu.np>
2025-05-11 22:19:15 +0545

    News

--- newsportal/fetchednews/serializers.py
+++ newsportal/fetchednews/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from .models import FetchedNews
  
 class FetchedNewsSerializer(serializers.ModelSerializer):
+    summary = serializers.CharField(source='description')
+    image = serializers.CharField(source='image_url')
+
     class Meta:
         model = FetchedNews
-        fields = ['id', 'title', 'summary', 'image']
-        read_only_fields = ['id'] 
+        fields = ['id', 'title', 'summary', 'image'] 
